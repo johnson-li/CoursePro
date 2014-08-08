a44ba0bff72fa9659246ed484db9e2c2c666c184
johnson-li <johnsonli1993@163.com>
2014-08-08 20:47:40 +0800

    get course table

--- demo.py
+++ demo.py
@@ -4,34 +4,46 @@ import urllib2
 import urllib
 import cookielib
 
+host = 'xk.fudan.edu.cn'
 hostUrl = 'http://xk.fudan.edu.cn/xk/'
+courseTableUrl = 'http://xk.fudan.edu.cn/xk/courseTableServlet'
 postUrl = 'http://xk.fudan.edu.cn/xk/loginServlet'
 imageUrl = "http://xk.fudan.edu.cn/xk/image.do"
 htmlFileSrc = 'index.html'
+courseFileSrc = 'course.html'
 imageFileSrc = 'image.jpg'
 
-cj = cookielib.LWPCookieJar()
-cookieSupport = urllib2.HTTPCookieProcessor(cj)
-opener = urllib2.build_opener(cookieSupport, urllib2.HTTPHandler)
-urllib2.install_opener(opener)
+cookie = cookielib.LWPCookieJar()
+cookieSupport = urllib2.HTTPCookieProcessor(cookie)
+urlOpener = urllib2.build_opener(cookieSupport)
+urlOpener.open(hostUrl)
+print cookie
 
 response = urllib2.urlopen(hostUrl)
 
-imageData = urllib.urlopen(imageUrl).read()
+imageData = urlOpener.open(imageUrl).read()
 imageFile = open(imageFileSrc, 'wb')
 imageFile.write(imageData)
 imageFile.close()
 
+print 'image file sore to ' + imageFile.name
 rand = raw_input()
 
-headers = {'User-Agent': '"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0"'}
+headers = {'User-Agent': '"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:31.0) Gecko/20100101 Firefox/31.0"',
+           'Refer': 'http://xk.fudan.edu.cn/xk/'}
 postData = {'studentId': '12307130211', 'password': '093418', 'Submit2': 'Submit', 'rand': rand}
 postData = urllib.urlencode(postData)
 
 request = urllib2.Request(postUrl, postData, headers)
-loginResponse = urllib2.urlopen(request)
+loginResponse = urlOpener.open(request)
 loginPage = loginResponse.read()
 
 htmlFile = open(htmlFileSrc, 'wb')
 htmlFile.write(loginPage)
 htmlFile.close()
+
+courseTable = urlOpener.open(courseTableUrl).read()
+courseFile = open(courseFileSrc, 'wb')
+courseFile.write(courseTable)
+courseFile.close()
+
